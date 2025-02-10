995bc8f9a90f61b6586f33921aba41e8fdf3409e
Francisco Echavarría <fjepaez@gmail.com>
2025-02-10 16:13:23 -0500

    workin in page 2 - amadeus

--- app/components/PerfilAutentificacion.tsx
+++ app/components/PerfilAutentificacion.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import { ReactElement } from 'react';
 import { useForm } from 'react-hook-form';
 import '../styles/PerfilAutentificacion.css';
 import Navegacion from './Navegacion';
@@ -10,6 +11,10 @@ const dotElement = useRef<HTMLSpanElement>(null);
 const [nombre, setNombre] = useState<string>('');
 const [correo, setCorreo] = useState<string>('');
 const [role, setRole] = useState('usuario');
+const [contrasena, setContrasena] = useState<string>('');
+
+
+
 
 // Función para manejar el cambio en el radio button
 const handleRoleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -37,14 +42,22 @@ const currentSlide = (n: number) => {
 const modificarNombre = (e: React.ChangeEvent<HTMLInputElement>) => {
 }
 
-const modificacorreo = (e: React.ChangeEvent<HTMLInputElement>) => {
+const modificarCorreo = (e: React.ChangeEvent<HTMLInputElement>) => {
 }
+
+const modificarConstrasena = (e: React.ChangeEvent<HTMLInputElement>) => {
+}
+
 
 const pushNombre = (e: React.KeyboardEvent<HTMLInputElement>) => {
 }
 
-const pushcorreo = (e: React.KeyboardEvent<HTMLInputElement>) => {
+const pushCorreo = (e: React.KeyboardEvent<HTMLInputElement>) => {
 }
+
+const pushContrasena = (e: React.KeyboardEvent<HTMLInputElement>) => {
+}
+
 
 
 
@@ -58,22 +71,43 @@ const pushcorreo = (e: React.KeyboardEvent<HTMLInputElement>) => {
     <Form className='container__main'>
       <div className="container__main__card">
         <section className="container__main__card__avatar">
-          <div ref= {slidesElements} className="container__main__card__avatar--img fade">
-            <img src="/assets/img/img-avatar/ava11.png" width="100%" />
-          </div>
-          <div ref= {slidesElements} className="container__main__card__avatar--img fade">
-            <img src="/assets/img/img-avatar/ava12.png" width="100%" />
-          </div>
-          
-          <div ref= {slidesElements} className="container__main__card__avatar--img fade">
-            <img src="/assets/img/img-avatar/ava13.png" width="100%" />
-          </div>
-          
-          <div ref= {slidesElements} className="container__main__card__avatar--img fade">
-            <img src="/assets/img/img-avatar/ava14.png" width="100%" />
-          </div>
-        </section>
+          { role === 'usuario' ? (
+            <>
+              <div ref= {slidesElements} className="container__main__card__avatar--img fade">
+                <img src="/assets/img/avatares/ava11.png" width="100%" />
+              </div>
+              <div ref= {slidesElements}
+                  className="container__main__card__avatar--img fade">
+                <img src="/assets/img/avatares/ava12.png" width="100%" />
+              </div>
+              
+              <div ref= {slidesElements} className="container__main__card__avatar--img fade">
+                <img src="/assets/img/avatares/ava13.png" width="100%" />
+              </div>
+              <div ref= {slidesElements} className="container__main__card__avatar--img fade">
+                <img src="/assets/img/avatares/ava14.png" width="100%" />
+              </div>
+              </>
+            ) : role === 'administrativo' ? (
+                <>
+              <div ref= {slidesElements} className="container__main__card__avatar--img fade">
+                <img src="/assets/img/avatares/ava21.png" width="100%" />
+              </div>
 
+              <div ref= {slidesElements} className="container__main__card__avatar--img fade">
+                <img src="/assets/img/avatares/ava22.png" width="100%" />
+              </div>
+              <div ref= {slidesElements}
+                  className="container__main__card__avatar--img fade">
+                <img src="/assets/img/avatares/ava23.png" width="100%" />
+              </div>
+              
+              <div ref= {slidesElements} className="container__main__card__avatar--img fade">
+                <img src="/assets/img/avatares/ava24.png" width="100%" />
+              </div>
+              </>
+          ): null}
+        </section>
         <section className="container__main__card__select">
           <a className="prev" onClick={() => plusSlides(-1)}>&#10094;</a>
           <div className="container__main__card__dot">
@@ -84,12 +118,17 @@ const pushcorreo = (e: React.KeyboardEvent<HTMLInputElement>) => {
           </div> 
           <a className="next" onClick={() => plusSlides(-1)}>&#10095;</a>
         </section>
-
         <section className="container__main__card__data">
           <h1 className='titulo-registrate'>Registrate</h1>     
           <input className="container__main__card__data--input" type="text" placeholder="Nombre" value = {nombre} onChange={modificarNombre} onKeyUp={pushNombre}/>
           <input className='container__main__card__data--input'
-          type='email' placeholder='Correo' value={correo} onChange={modificacorreo} onKeyUp={pushcorreo}/>
+          type='email' placeholder='Correo' value={correo} onChange={modificarCorreo} onKeyUp={pushCorreo}/>
+            {role === 'administrativo' ? (
+              <>
+                <input className='container__main__card__data--input'
+                type='password' placeholder='Contraseña' value={contrasena} onChange={modificarConstrasena} onKeyUp={pushContrasena}/>
+              </>
+            ):null}          
           <div className="contenedor-botones-1">
             <div className="contenedro-botones-1-1">
               <input
@@ -101,6 +140,7 @@ const pushcorreo = (e: React.KeyboardEvent<HTMLInputElement>) => {
                 checked={role === 'usuario'}
                 onChange={handleRoleChange}
               />
+            
               <label className="botones-1-1-label" htmlFor="usuario">
                 usuario
               </label>
@@ -120,11 +160,21 @@ const pushcorreo = (e: React.KeyboardEvent<HTMLInputElement>) => {
               </label>
             </div>
           </div>
-
+          <div className="container__main__card__data--personalData">
+              <input 
+              type="checkbox" 
+              name="data-accepted" 
+              id="data-accepted"/>
+              <label 
+                className="container__main__card__data--checkbox" 
+                htmlFor="data-accepted"> 
+                Acepto los 
+                <a href="https://amadeus.com/es/politicas/privacy-policy" 
+                target="_blank">términos y condiciones
+                </a> de la política de protección de datos.
+              </label>
+          </div>
         </section>
-
-
-
       </div>
     </Form>
     </>
