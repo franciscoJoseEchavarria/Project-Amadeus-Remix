74f6305229acbdb48adf90c05358450ae33fddc1
Francisco Echavarría <fjepaez@gmail.com>
2025-02-16 20:00:21 -0500

    Solution to Css with library tailwind.css

--- app/components/Navegacion.tsx
+++ app/components/Navegacion.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import '../styles/Navegacion.css';
 import React from 'react';
+import "app/tailwind.css";
 import 'app/styles.css';
 
 const Navegacion: React.FC = () => {
@@ -10,10 +11,10 @@ const Navegacion: React.FC = () => {
             <img className ="nav__logo" src="../public/assets/amadeus-logo-dark-sky.png" alt="logo" />
             <div className ="nav__container">
                 <ul className="nav__lista">
-                    <li><Link to= "/">Inicio</Link></li>
-                    <li><Link  to="https://amadeus.com/es/contacto" target="_blank" className= "nav__link">Contacto</Link></li>
-                    <li><Link to= "" className= "nav__link">Reporte</Link></li>
-                    <li><a href="https://amadeus.com/en" target="_blank">Amadeus</a></li>
+                    <li className='pokerface'><Link to= "/">Inicio</Link></li>
+                    <li className='pokerface'><Link  to="https://amadeus.com/es/contacto" target="_blank" className= "nav__link">Contacto</Link></li>
+                    <li className='pokerface'><Link to= "" className= "nav__link">Reporte</Link></li>
+                    <li className='pokerface'><a href="https://amadeus.com/en" target="_blank">Amadeus</a></li>
                 </ul>
             </div>
         </nav>
